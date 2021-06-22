817cbf1889d16cb749a74369a2706d10e628c93e
Dipak Anand <deepak@annamrajudesigns.com>
2021-06-22 19:13:28 +0530

    Skipper HOmepAge

---
+++ wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'skippermetzer' );
+
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'TY*Fqp&l6b/0T@a#MR>w!obTZK9SI 9o5ho#GfIX*f{F~:T$*<qMo<BIWW$Wqj@K' );
+define( 'SECURE_AUTH_KEY',  '_5D&:S<:+s;&3sden _*3~b%4SmQN5@7KCA.9FgPdc>wRkfpp@U5`7vAp$!N%Ka6' );
+define( 'LOGGED_IN_KEY',    'j?[(^;t`lmG@b5Q3~56PMvzKq93jeZUQcPdkqsuo4u$b`*?dAe{`3yY1RG!B:yiG' );
+define( 'NONCE_KEY',        'Fqeu`rT0I(~lv1SCNC!P=6/fzT6E3H((RwAh`^s}p5g+N<##22@Xq40O>?`KWA4)' );
+define( 'AUTH_SALT',        ';ez6ldzb<(d}4-6><W=V(OBKkdlFeE0oTMXZ<Nj];nz-*o98@Qzb]BI,=7:51WC0' );
+define( 'SECURE_AUTH_SALT', '@eg<@DhlfZmh fo,<S0LYG*e2- iUK~=WT`g?i}mHXJM4N?BAPZ,N)1ss)b8=>)Y' );
+define( 'LOGGED_IN_SALT',   '>ohu64LbWv*u%O2Z&/xU)[*a:^17[|1%MR918G(1^y4y}1ybEdPHC,rdfp.z,e5f' );
+define( 'NONCE_SALT',       'r(]jRcsM@P.x6~`k%4lX@9O SGT7UD6.mm;5$u(dd=o0-JA,/W2[&-,cB6zA] `r' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/themes/SkipperMetzer/index.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Skipper Metzer</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="/wp-content/themes/SkipperMetzer/Css/Style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f3aebf1c73.js" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                defaultLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <style>
+        .goog-te-gadget-simple {
+            background-color: #fff;
+            border-left: none;
+            border-top: none;
+            border-bottom: none;
+            border-right: none;
+            font-size: 12px;
+            display: inline-block;
+            padding-top: 0px;
+            padding-bottom: 0px;
+            cursor: pointer;
+            zoom: 1;
+            display: inline;
+        }
+
+        .goog-te-gadget img {
+            display: none;
+        }
+
+        .goog-te-menu-value span {
+            color: red;
+        }
+
+        .goog-te-menu-value span:nth-child(3) {
+            display: none;
+        }
+
+        .goog-te-menu-value span:nth-child(5) {
+            display: none;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="bg-white mainNav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="#"><img src="/wp-content/themes/SkipperMetzer/Images/logo.png" alt="Skipper Metzer" class="w-100"></a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div class="navbar-nav ms-auto text-uppercase">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                        <a class="nav-link" href="#">About us</a>
+                                        <a class="nav-link" href="#">Products</a>
+                                        <a class="nav-link" href="#">Gallery</a>
+                                        <a class="nav-link" href="#">Contact us</a>
+                                        <a class="nav-link" href="#">
+                                            <div id="google_translate_element"></div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <section class="mainBanner">
+                <footer class="bannerFooter">
+                    <div class="footerbox">
+                        <img src="/wp-content/themes/SkipperMetzer/Images/Logo-photo/flags.png" alt="" class="img-responsive" style="max-width:100px">
+                        <p class="ps-2">AN-INDO ISRAELI JOINT VENTURE SMART IRRIGATION SOLUTIONS</p>
+                    </div>
+                </footer>
+            </section>
+            <section class="viewSector mt-5">
+               <div class="container-fluid">
+                   <div class="row">
+                        <div class="col-md-3">
+                            <div class="rowCrops">
+                                <div class="innerField">
+                                    <h4>Row Crops</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="orchards">
+                                <div>
+                                    <h4>Orchards</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="greenHouses">
+                                <div>
+                                    <h4>Green Houses</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="landScaping">
+                                <div>
+                                    <h4>Land Scaping</h4>
+                                </div>
+                            </div>
+                        </div>
+                   </div>
+               </div>
+            </section>
+            <section class="aboutUSback mt-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center py-5">
+                            <h2 class="text-white">About <span>Us</span></h2>
+                            <p>Skipper Metzer India LLP is an Indo-Israeli joint venture between M/s Skipper 
+                            Limited, India and M/s Metzer, Israel, to foray into Micro Irrigation market in India with 
+                            an objective to become a key player and bring in new technologies in the growing 
+                            irrigation market to help farmers.
+
+                            Skipper Metzer Head Office and production facilities with state of the art “Israeli 
+                            Technologies” are located in Hyderabad.
+                            Skipper Metzer is led by experienced team members with more than two and half 
+                            decades of experience in Micro Irrigation...
+                            </p>
+                            <a href="#" class="btn readmoreBtn mt-5">Read More<i class="fas fa-long-arrow-alt-right ms-3"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="mainfooter mt-5">
+            <div class="container-fluid">
+                <div class="row ">
+                    <div class="col-md-3 ">
+                        <div class="innerBox">
+                            <img src="/wp-content/themes/SkipperMetzer/Images/Logo-photo/flags.png" alt="" class="img-responsive" style="max-width:260px" />
+                            <p><strong>Skipper Metzer India LLP</strong> is an Indo-Israeli joint venture between M/s Skipper Limited, India
+                                and M/s Metzer, Israel, to foray into Micro Irrigation
+                                market in India with an objective to become a key
+                                player and bring in new technologies in the
+                                growing irrigation market to help farmers.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 ">
+                        <div class="innerBox">
+                            <h4>Useful Links</h4>
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/">About Us </a></li>
+                                <li><a href="/">Products</a></li>
+                                <li><a href="/">Gallery</a></li>
+                                <li><a href="/">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 ">
+                        <div class="innerBox">
+                            <h4>Our Products</h4>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <ul>
+                                        <li><a href="/">LIN</a></li>
+                                        <li><a href="/">Vardit Pc Dripper </a></li>
+                                        <li><a href="/"> Adi</a></li>
+                                        <li><a href="/">Compact </a></li>
+                                        <li><a href="/">Online Drippers </a></li>
+                                        <li><a href="/">Brass Sprinklers </a></li>
+                                        <li><a href="/">Compact Drip Laterals </a></li>
+                                        <li><a href="/">Disc Filter </a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <ul>
+                                        <li><a href="/">Gali </a></li>
+                                        <li><a href="/">HDPC Pipes & Fittings </a></li>
+                                        <li><a href="/">Hydro Cyclone Filter</a></li>
+                                        <li><a href="/">Lin Drip Line</a></li>
+                                        <li><a href="/">Gravel Filter</a></li>
+                                        <li><a href="/">Mini Sprinkler</a></li>
+                                        <li><a href="/">Screen Filter</a></li>
+                                        <li><a href="/">Lateral 32mm</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-3 ">
+                        <div class="innerBox">
+                            <h4>Contact Us</h4>
+                            <address>
+                                <p class="addresss"><i class="fas fa-map-marker-alt"></i>
+                                    Skipper Metzer India LLP
+                                    44-67, Sy. No 367/1/A, IDA, Moula-Ali,
+                                    Hyderabad-500040, Telangana, India|
+                                </p>
+                                <p class="addresss">
+                                    <i class="fas fa-phone-alt"></i>
+                                    +91 40 27243427, +91 9989773604
+                                </p>
+                            </address>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="border-bottom w-100"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="innerBox">
+                            <address>
+                                <p><i class="fas fa-envelope me-3"></i> customercare@skippermetzer.com</p>
+                            </address>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="innerBox text-center">
+                            <h4 class="text-info">Call Us On</h4>
+                            <p class="text-white fs-3">18005722997</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
